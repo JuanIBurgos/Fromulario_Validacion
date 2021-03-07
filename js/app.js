f303b537eb86b466422853a8e89db096e8cc2d45
JuaniBurgos <juanburgos86b@gmail.com>
2021-03-07 16:39:03 -0300

    Agrego el validarGeneral, el cual valido el botón Enviar. Se agrado los Script de EmailJS

--- js/app.js
+++ js/app.js
@@ -5,8 +5,10 @@ function campoRequerido(input) {
     //Preguntamos si  es un campo vacio
     if (input.value.trim() === "") {
         input.className = "form-control is-invalid";
+        return false;
     } else {
         input.className = "form-control is-valid";
+        return true;
     }
 }
 
@@ -17,18 +19,22 @@ function validarMail(mail) {
     //console.log("Holis desde validarMail")
     if (mail.value.trim() != "" && expresion.test(email.value)) {
         mail.className = "form-control is-valid";
+        return true;
     } else {
         mail.className = "form-control is-invalid";
+        return false;
     }
 }
 
 //Función que valida el teléfono
 function validarTelefono(input) {
-    console.log("Holis desde validarTelefono.");
+    //console.log("Holis desde validarTelefono.");
     if (input.value.trim() != "" && !isNaN(input.value)) {
         input.className = "form-control is-valid";
+        return true;
     } else {
         input.className = "form-control is-invalid";
+        return false;
     }
 
 }
@@ -39,9 +45,11 @@ function validarConsulta(input) {
     //console.log(input.value.length);
     if (input.value.trim() != "" && input.value.length >= 10) {
         input.className = "form-control is-valid";
+        return true;
 
     } else {
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 
@@ -53,7 +61,7 @@ function contarCaracteres(input) {
 
 //Validamos el cheknox de novedades
 let checkTerminos = document.getElementById('checkNovedades');
-//uso un manejador de eventos y funciones anónimas
+//uso un manejador de eventos junto a una función anónima y una normal para reutilizarla a  futuro
 checkTerminos.addEventListener('change', function() {
     validarTerminos(checkTerminos);
 })
@@ -61,7 +69,25 @@ checkTerminos.addEventListener('change', function() {
 function validarTerminos(checkTerminos) {
     if (checkTerminos.checked) {
         checkTerminos.className = "form-check-input is-valid";
+        return true;
     } else {
         checkTerminos.className = "form-check-input is-invalid";
+        return false;
+    }
+}
+
+//Validiamos todo el formulario - a traves del botón enviar
+function validarGeneral(event) {
+    console.log("Holis desde validarGeneral");
+    event.preventDefault();
+    if (campoRequerido(document.querySelector('#nombre')) &&
+        validarMail(document.querySelector('#email')) &&
+        validarConsulta(document.querySelector('#consulta')) &&
+        validarTelefono(document.querySelector('#telefono')) &&
+        validarTerminos(checkTerminos)) {
+        console.log("Holis Verdadero");
+
+    } else {
+        console.log("Holis falso");
     }
 }
